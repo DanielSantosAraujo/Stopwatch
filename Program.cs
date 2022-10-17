6c1d2ae3d6cbec75705d25c27e9b69110ebac722
Daniel Santos <dsaraujo718@gmail.com>
2022-10-17 16:29:49 -0300

    Adicionando menu de operações

--- Program.cs
+++ Program.cs
@@ -5,12 +5,34 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
+            
+        }
+
+        
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -20,6 +42,11 @@ namespace Stopwatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
